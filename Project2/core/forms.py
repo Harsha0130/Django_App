d1ea76f3d8f244b419cb9179842532381314e2de
Harsha0130 <harshahm3304@gmail.com>
2025-02-01 22:47:23 +0530

    Login

--- Project2/core/forms.py
+++ Project2/core/forms.py
@@ -1,6 +1,18 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-',
+    }))
+
+    
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your email address',
+        'class': 'w-full py-4 px-6 rounded-',
+    }))
 
 class SignupForm(UserCreationForm):
     class Meta:
